12387bb840b42bc5837da4a71532e00310c3cefb
hariprasad2512 <hprasad887@gmail.com>
2026-07-17 21:29:12 +0530

    Added UserAuth Views and URLs in Tracker

--- tracker/urls.py
+++ tracker/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     # We will add our Auth, Dashboard, and CRUD routes here next
+    path('/register', view=views.register, name='register'),
+    path('login/', views.login_view, name='login_view'),
+    path('logout/', views.logout_view, name='logout_view'),
 ]
--- tracker/views.py
+++ tracker/views.py
@@ -1,4 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+#|Register View
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            # We will create the 'dashboard' URL in Phase 3
+            return redirect('dashboard') 
+    else:
+        form = UserCreationForm()
+    
+    return render(request, 'tracker/register.html', {'form': form})
 
-# Create your views here.
+# Login View and Logout View
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                # We will create the 'dashboard' URL in Phase 3
+                return redirect('dashboard')
+    else:
+        form = AuthenticationForm()
+        
+    return render(request, 'tracker/login.html', {'form': form})
 
+def logout_view(request):
+    logout(request)
+    return redirect('login_view')
